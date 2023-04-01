ae1ef74e28a87fad635561305f1d3f0a41bc34b1
tanu31195 <tanu31195@users.noreply.github.com>
2023-04-01 19:33:50 +0530

    Added drawer and nested navigation

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+        drawerContentStyle: { backgroundColor: "#351401" },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#351401",
+        drawerActiveBackgroundColor: "#e4baa1",
+      }}
+    >
+      <Drawer.Screen
+        name='Categories'
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons color={color} size={size} name='list' />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name='Favorites'
+        component={FavoritesScreen}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons color={color} size={size} name='star' />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -21,22 +63,16 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name='MealCategories'
-            component={CategoriesScreen}
+            // name='MealCategories'
+            // component={CategoriesScreen}
+            name='Drawer'
+            component={DrawerNavigator}
             options={{
-              title: "All Categories",
+              // title: "All Categories",
+              headerShown: false,
             }}
           />
-          <Stack.Screen
-            name='MealsOverview'
-            component={MealsOverviewScreen}
-            // options={({route, navigation}) => {
-            //   const title = route.params.title
-            //   return {
-            //     title: title
-            //   }
-            // }}
-          />
+          <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
           <Stack.Screen
             name='MealDetail'
             component={MealDetailScreen}
---
+++ README.md
@@ -0,0 +1,2 @@
+https://tanu31195.github.io/data/react-native/meals-app/categories.json
+https://tanu31195.github.io/data/react-native/meals-app/meals.json
